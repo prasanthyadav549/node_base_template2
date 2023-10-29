c3bd7b3c214cce00ce54d4b9d314032be7d2b8d6
prasanthyadav549 <prasanthyadav549@gmail.com>
2023-10-29 06:36:49 +0530

    added the middleware for createCity

--- src/routes/v1/city-routes.js
+++ src/routes/v1/city-routes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const { CityController } = require("../../controllers");
+const { CityMiddlewares } = require("../../middlewares");
 const router = express.Router();
 console.log("inside the city routes");
 
 // /api/v1/city
-router.post("/", CityController.createCity);
+router.post(
+  "/",
+  CityMiddlewares.validateCreateRequest,
+  CityController.createCity
+);
 module.exports = router;
